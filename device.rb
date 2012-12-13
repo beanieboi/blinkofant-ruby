2e9f40e2993407bd093e1063e7e99c3b41912cc0
beanieboi <beanie@benle.de>
2012-12-13 21:26:54 +0100

    setting clock speed via ioctl

--- device.rb
+++ device.rb
@@ -2,16 +2,18 @@ require 'fcntl'
 
 module Blinkofant
   class Device
+    SPI_IOC_WR_MAX_SPEED_HZ = 0x40046b04
 
     def initialize
       @device = '/dev/spidev0.0'
-      @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
     end
 
     def flush(screen)
-      f = IO.open(fd)
-      f.ioctl(4, 100000)
-      f.syswrite(screen.bit_stream)
+      @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
+      f = IO.open(@fd)
+      f.sync = true
+      f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
+      f.write(screen.bit_stream.pack("C*"))
       f.close
     end
   end
