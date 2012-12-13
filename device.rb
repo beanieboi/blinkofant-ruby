8c12a078143bdc68e13e7a4e00f9b0cec3e0bd76
beanieboi <beanie@benle.de>
2012-12-14 00:55:25 +0100

    remove pong related stuff

--- device.rb
+++ device.rb
@@ -1,31 +1,20 @@
 require 'fcntl'
-require 'wiringpi'
 
 module Blinkofant
   class Device
     SPI_IOC_WR_MAX_SPEED_HZ = 0x40046b04
-    DARKEN_PIN = 22
 
     def initialize
       @device = '/dev/spidev0.0'
-
-      @io = WiringPi::GPIO.new
-      @io.mode(DARKEN_PIN,OUTPUT)
     end
 
     def flush(screen)
-      @io.write(DARKEN_PIN,LOW)
-      @io.read(DARKEN_PIN)
-      
       @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
       f = IO.open(@fd)
       f.sync = true
       f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
       f.write(screen.bit_stream.pack("C*"))
       f.close
-
-      @io.write(DARKEN_PIN,HIGH)
-      @io.read(DARKEN_PIN)
     end
   end
 end
