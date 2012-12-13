b7bfc53e6043146bf6469e4b709d9eaa4a0d7abd
beanieboi <beanie@benle.de>
2012-12-13 21:27:06 +0100

    adding test programm

--- test.rb
+++ test.rb
@@ -1,1 +1,14 @@
-test.rb
+require './screen'
+require './device'
+
+d = Blinkofant::Device.new
+s = Blinkofant::Screen.new
+
+s.row(1, true)
+d.flush(s)
+
+while true do
+  s.shift_rows
+  d.flush(s)
+  sleep(1)
+end
