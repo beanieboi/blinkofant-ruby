5fd2074cc522b8f6cb2a8efcae74189657c8a019
beanieboi <beanie@benle.de>
2012-12-15 20:45:45 +0100

    put Device into Blinkofant::Device

--- device.rb
+++ device.rb
@@ -1,26 +1,28 @@
 require 'wiringpi'
 require 'fcntl'
 
-class Device
-  SPI_IOC_WR_MAX_SPEED_HZ = 0x40046b04
-  DARKEN_PIN = 6
+module Blinkofant
+  class Device
+    SPI_IOC_WR_MAX_SPEED_HZ = 0x40046b04
+    DARKEN_PIN = 6
 
-  def initialize(screen)
-    @screen = screen
-    @device = '/dev/spidev0.0'
-    @io = WiringPi::GPIO.new
-    @io.mode(DARKEN_PIN,OUTPUT)
-  end
+    def initialize(screen)
+      @screen = screen
+      @device = '/dev/spidev0.0'
+      @io = WiringPi::GPIO.new
+      @io.mode(DARKEN_PIN,OUTPUT)
+    end
 
-  def flush
-    @io.write(DARKEN_PIN,LOW)
-    @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
-    f = IO.open(@fd)
-    f.sync = true
-    f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
-    f.write(@screen.bit_stream.pack("C*"))
-    f.close
-    @screen.reset
-    @io.write(DARKEN_PIN,HIGH)
+    def flush
+      @io.write(DARKEN_PIN,LOW)
+      @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
+      f = IO.open(@fd)
+      f.sync = true
+      f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
+      f.write(@screen.bit_stream.pack("C*"))
+      f.close
+      @screen.reset
+      @io.write(DARKEN_PIN,HIGH)
+    end
   end
 end
